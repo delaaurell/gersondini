f0a733f3e29f17aa734820cadc8eed8e266e8fa9
delaaurell <54821972+delaaurell@users.noreply.github.com>
2023-05-26 10:42:10 +0700

    Initial Commit

---
+++ gersondini/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  gersondini
+//
+//  Created by Ida Ayu Dela Aurellia on 26/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gersondini/gersondiniApp.swift
@@ -0,0 +1,17 @@
+//
+//  gersondiniApp.swift
+//  gersondini
+//
+//  Created by Ida Ayu Dela Aurellia on 26/05/23.
+//
+
+import SwiftUI
+
+@main
+struct gersondiniApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

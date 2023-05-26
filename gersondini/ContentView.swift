1012b906ea558783d5139421c05eaf2ca1cd512d
delaaurell <54821972+delaaurell@users.noreply.github.com>
2023-05-26 10:49:01 +0700

    nambah listpage

--- gersondini/ContentView.swift
+++ gersondini/ContentView.swift
@@ -1,26 +1,70 @@
 //
 //  ContentView.swift
-//  gersondini
+//  GIT_Collaboration_Test
 //
-//  Created by Ida Ayu Dela Aurellia on 26/05/23.
+//  Created by Gerson Janhuel on 15/05/23.
 //
 
 import SwiftUI
 
+// Orang ke 1
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+                
+                ListRowView()
+            }
+            .listStyle(.plain)
+            .navigationTitle("Awesome List")
         }
-        .padding()
+        
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct ListRowView: View {
+struct ListRowView: View {
+    var body: some View {
+        NavigationLink(destination: DetailView()) {
+            // orang kedua
+            HStack(alignment: .top) {
+                Rectangle()
+                    .frame(width: 80, height: 80)
+                    .foregroundColor(.blue)
+                
+                VStack(alignment: .leading) {
+                    Rectangle()
+                        .frame(width: 80, height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                    
+                    Rectangle()
+                        .frame(height: 20)
+                }
+                .foregroundColor(.orange)
+            }
+            .padding(.vertical, 10)
+        }
     }
 }
